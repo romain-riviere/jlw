bed8fad7cb32f13b568e672cfb5b838566105007
Romain Riviere <rom.riviere7@gmail.com>
2024-05-09 14:53:56 +0200

    feat: added design & picutres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 
 import { Component } from "@angular/core";
 import { CounterComponent } from "./counter/counter.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CounterComponent]
+    imports: [CounterComponent, GalleryComponent]
 })
 export class AppComponent {
 
